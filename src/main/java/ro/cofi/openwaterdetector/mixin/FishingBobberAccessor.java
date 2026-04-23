68d4c67e65dee2f3bf74acd0e2d4cf1837372521
AZhong41241214 <a0945678900@gmail.com>
2026-04-23 16:59:07 +0800

    Update FishingBobberAccessor.java

--- src/main/java/ro/cofi/openwaterdetector/mixin/FishingBobberAccessor.java
+++ src/main/java/ro/cofi/openwaterdetector/mixin/FishingBobberAccessor.java
@@ -2,15 +2,11 @@ package ro.cofi.openwaterdetector.mixin;
 
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(FishingBobberEntity.class)
-public abstract class FishingBobberAccessor {
+public interface FishingBobberAccessor {
 
-    @Shadow
-    private FishingBobberEntity.State state;
-
-    public FishingBobberEntity.State getState() {
-        return this.state;
-    }
+    @Invoker("isOpenOrWaterAround")
+    boolean invokeIsOpenOrWaterAround(net.minecraft.util.math.BlockPos pos);
 }
